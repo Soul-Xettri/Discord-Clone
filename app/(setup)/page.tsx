625c999544691c31bdd2281776280f29383ca60e
sou-xettri <rakeshchhetri420@gmail.com>
2023-12-10 16:40:57 +0545

    initial modal ui added to the code

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,3 +1,4 @@
+import { InitialModal } from "@/components/modals/initial-modal";
 import { db } from "@/lib/db";
 import { initialProfile } from "@/lib/initial-profile";
 import { redirect } from "next/navigation";
@@ -6,20 +7,24 @@ const SetupPage = async () => {
   const profile = await initialProfile();
 
   const server = await db.server.findFirst({
-    where:{
-        members:{
-            some:{
-                profileId: profile.id
-            }
-        }
-    }
-  })
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
 
-  if(server){
-    return redirect(`/servers/${server.id}`)
+  if (server) {
+    return redirect(`/servers/${server.id}`);
   }
 
-  return <div>Create a Server</div>;
+  return (
+    <div>
+      <InitialModal />
+    </div>
+  );
 };
 
 export default SetupPage;
---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+
+const formScheme = z.object({
+  name: z.string().min(1, {
+    message: "Server name is required",
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is required",
+  }),
+});
+
+export const InitialModal = () => {
+
+    // for hydration error
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(()=>{
+    setIsMounted(true)
+  },[])
+
+  const form = useForm({
+    resolver: zodResolver(formScheme),
+    defaultValues: {
+      name: "",
+      imageUrl: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formScheme>) => {
+    console.log(values);
+  };
+  if(!isMounted){
+    return null;
+  }
+  return (
+    <div>
+      <Dialog open>
+        <DialogContent className="bg-white text-black p-0 overflow-hidden">
+          <DialogHeader className="pt-8 px-6">
+            <DialogTitle className="text-2xl text-center">
+              Customize your server
+            </DialogTitle>
+            <DialogDescription className="text-center text-zinc-500">
+              Give your server a personality with a name and an image. You can
+              always change it later.
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <div className="space-y-8 px-6">
+                <div className="flex items-center justify-center text-center">
+                  TODO :: Image Upload
+                </div>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                        Server name
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          disabled={isLoading}
+                          className="bg-zinc-300/50 border-50 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                          placeholder="Enter server name"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage/>
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <DialogFooter className="bg-gray-100 px-6 py-4">
+                  <Button disabled={isLoading} variant="primary">
+                    Create
+                  </Button>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
