700c71bbee99ca4c0b5f06b232283875720c90fd
sou-xettri <rakeshchhetri420@gmail.com>
2023-12-16 19:44:49 +0545

    Create Server Modal

--- components/action-tooltip.tsx
+++ components/action-tooltip.tsx
@@ -1,32 +1,37 @@
 "use client";
 
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-interface ActionTooltipProps{
-    label:string;
-    children:React.ReactNode;
-    side?:"top"|"right"|"bottom"|"left";
-    align?:"start"|"center"|"end";
+interface ActionTooltipProps {
+  label: string;
+  children: React.ReactNode;
+  side?: "top" | "right" | "bottom" | "left";
+  align?: "start" | "center" | "end";
 }
 
-export const ActionTooltip =({label,children,side,align}:ActionTooltipProps)=>{
-    return(
-        <TooltipProvider>
-            <Tooltip delayDuration={50}>
-                <TooltipTrigger asChild>
-                    {children}
-                </TooltipTrigger>
-                <TooltipContent side={side} align={align}>
-                    <p className="font-semibold text-sm capitalize">
-                        {label.toLowerCase()}
-                    </p>
-                </TooltipContent>
-            </Tooltip>
-        </TooltipProvider>
-    )
-}
+export const ActionTooltip = ({
+  label,
+  children,
+  side,
+  align,
+}: ActionTooltipProps) => {
+  return (
+    <div>
+      <TooltipProvider>
+        <Tooltip delayDuration={50}>
+          <TooltipTrigger asChild>{children}</TooltipTrigger>
+          <TooltipContent side={side} align={align}>
+            <p className="font-semibold text-sm capitalize">
+              {label.toLowerCase()}
+            </p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
+};
--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -16,30 +16,32 @@ export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
   const params = useParams();
   const router = useRouter();
 
-  const onClick = ()=>{
-    router.push(`/server/${id}`)
-  }
+  const onClick = () => {
+    router.push(`/server/${id}`);
+  };
 
   return (
-    <ActionTooltip side="right" align="center" label={name}>
-      <button onClick={onClick} className="group relative flex items-center">
-        <div
-          className={cn(
-            "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
-            params?.serverId !== id && "group=hover:h-[20px]",
-            params?.serverId === id ? "h-[36px]" : "h-[38px]"
-          )}
-        />
-        <div
-          className={cn(
-            "relative group flex m-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
-            params?.serverId === id &&
-              "bg-primary/10 text-primary rounded-[16px]"
-          )}
-        >
-          <Image fill src={imageUrl} alt="channel" />
-        </div>
-      </button>
-    </ActionTooltip>
+    <div>
+      <ActionTooltip side="right" align="center" label={name}>
+        <button onClick={onClick} className="group relative flex items-center">
+          <div
+            className={cn(
+              "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+              params?.serverId !== id && "group=hover:h-[20px]",
+              params?.serverId === id ? "h-[36px]" : "h-[38px]"
+            )}
+          />
+          <div
+            className={cn(
+              "relative group flex m-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+              params?.serverId === id &&
+                "bg-primary/10 text-primary rounded-[16px]"
+            )}
+          >
+            <Image fill src={imageUrl} alt="channel" />
+          </div>
+        </button>
+      </ActionTooltip>
+    </div>
   );
 };
