0f70ff62f0ababf3aac05ed2ac99a109716850d5
sou-xettri <rakeshchhetri420@gmail.com>
2023-12-19 03:59:00 +0545

    Server Settings

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,8 @@
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
 import { InviteModal } from "@/components/modals/invite-modal";
+import { EditServerModal } from "@/components/modals/edit-server-modal";
+
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -19,6 +21,7 @@ export const ModalProvider = () => {
     <div>
       <CreateServerModal />
       <InviteModal/>
+      <EditServerModal/>
     </div>
   );
 };
