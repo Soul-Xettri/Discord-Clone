b427beb953c02d02f9a1066fe0029f4acc152d1a
sou-xettri <rakeshchhetri420@gmail.com>
2023-12-23 14:17:29 +0545

    fixed search width

--- components/Server/server-search.tsx
+++ components/Server/server-search.tsx
@@ -67,7 +67,7 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
         className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full
        hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
       >
-        <Search className="h-4 m-4 text-zinc-500 dark:text-zinc-400" />
+        <Search className="h-4 w-4 text-zinc-500 dark:text-zinc-400" />
         <p
           className="font-semibold text-sm text-zinc-500 dark:text-zinc-400
            group-hover:text-zinc-600 dark:group-hover:text-zinc-300 transition"
