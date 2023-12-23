10b42ac574191b6504f4cb2f8617ef07d62eaf34
sou-xettri <rakeshchhetri420@gmail.com>
2023-12-23 15:03:19 +0545

    navigation sidebar bg color added and server-channel on hover delete label added

--- components/Server/server-channel.tsx
+++ components/Server/server-channel.tsx
@@ -55,7 +55,7 @@ export const ServerChannel = ({
                     dark:hover:text-zinc-300 transition"
               />
             </ActionTooltip>
-            <ActionTooltip label="Edit">
+            <ActionTooltip label="delete">
               <Trash
                 className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400
                     dark:hover:text-zinc-300 transition"
