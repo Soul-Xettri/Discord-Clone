acbd37e6dc2f44d48f10d2c079c29ca0f65c00b1
sou-xettri <rakeshchhetri420@gmail.com>
2023-12-23 17:18:25 +0545

    Chat Header part-1 and mobile toggle buger added

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,51 @@
-const ChannelIdPage = () => {
-  return <div>channel Id page</div>;
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/currentProfile";
+import { db } from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ChannelIdPageProps {
+  params: {
+    serverId: string;
+    channelId: string;
+  };
+}
+
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile.id,
+    },
+  });
+
+  if(!channel || !member){
+    redirect("/");
+  }
+
+  return (
+  <div 
+  className="bg-white dark:bg-[#313338] flex flex-col h-full"
+  >
+    <ChatHeader
+    name={channel.name}
+    serverId={params.serverId}
+    type="channel"
+    />
+  </div>
+  )
 };
 
 export default ChannelIdPage;
