4bc693723c71943f2afb025fa8f89f7f18395dd7
sou-xettri <rakeshchhetri420@gmail.com>
2023-12-24 02:28:44 +0545

    code formatted

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,22 +1,29 @@
-import { Hash  } from "lucide-react";
+import { Hash } from "lucide-react";
 import { MobileToggle } from "../mobile-toggle";
 
 interface ChatHeaderProps {
   serverId: string;
   name: string;
   type: "channel" | "conversation";
-  image?: string; 
+  image?: string;
 }
 
-export const ChatHeader = ({serverId,name,type,image}:ChatHeaderProps) => {
-  return (<div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200
-  dark:border-neutral-800 border-b-2">
-    <MobileToggle serverId={serverId}/>
-    {type === "channel" && (
-        <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2"/>
-    )}
-    <p className="font-semibold text-md text-black dark:text-white">
-        {name}
-    </p>
-  </div>);
+export const ChatHeader = ({
+  serverId,
+  name,
+  type,
+  image,
+}: ChatHeaderProps) => {
+  return (
+    <div
+      className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200
+  dark:border-neutral-800 border-b-2"
+    >
+      <MobileToggle serverId={serverId} />
+      {type === "channel" && (
+        <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      <p className="font-semibold text-md text-black dark:text-white">{name}</p>
+    </div>
+  );
 };
